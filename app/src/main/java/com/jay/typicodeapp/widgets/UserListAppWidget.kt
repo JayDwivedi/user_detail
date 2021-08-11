1a695e562ac4cdf45ddf0ba40981fca76b9f4571
Jay Dwivedi <jaidwivedi20@gmail.com>
2021-08-11 17:13:43 +0530

    Set alignment and add text view for empty list

--- app/src/main/java/com/jay/typicodeapp/widgets/UserListAppWidget.kt
+++ app/src/main/java/com/jay/typicodeapp/widgets/UserListAppWidget.kt
@@ -38,7 +38,7 @@ internal fun updateUserListAppWidget(
     val widgetText = context.getString(R.string.appwidget_text)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.user_list_app_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    views.setTextViewText(R.id.tvGreetings, widgetText)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
